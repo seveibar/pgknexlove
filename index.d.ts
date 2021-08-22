3a9bbb09970a1fa24326e883b8dc5ad7b77a4f45
seveibar <seveibar@gmail.com>
2021-08-21 22:32:31 -0700

    fix: initial types file

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+import { Knex } from "knex"
+
+type DatabaseGetter = {
+  getConnectionInfo: (
+    database?: string,
+    user?: string
+  ) => {
+    host: string
+    user: string
+    port: number
+    password: string
+    database: string
+    ssl: boolean
+  }
+  (params?: {
+    seed?: boolean
+    migrate?: boolean
+    testMode?: boolean
+    user?: string
+  }): Promise<Knex>
+}
+
+type GetDatabaseGetter = {
+  default: DatabaseGetter
+  (params?: {
+    migrationFile?: string
+    seedFile?: string
+    migrationSQL?: string
+    seedSQL?: string
+    pool?: number
+    defaults?: any
+  }): DatabaseGetter
+}
+
+export declare const getDatabaseGetter: GetDatabaseGetter
+
+export default getDatabaseGetter
